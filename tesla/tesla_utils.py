21511bdb80e02fb35e2d469e54971dd7a43c1362
cudderson <codered1227@gmail.com>
2021-02-15 15:10:16 -0600

    split functions for better design

--- tesla/tesla_utils.py
+++ tesla/tesla_utils.py
@@ -20,8 +20,8 @@ def current_price():
     return res['c']
 
 
-def create_candle_chart():
-    """Returns a candlestick chart in HTML format using a stock API and Pandas/Plotly"""
+def gather_candle_data():
+    """Gathers the data needed to make a candlestick chart"""
 
     # defining time range for api call (in UNIX)
     current_time_unix = int(time.time())
@@ -34,6 +34,79 @@ def create_candle_chart():
 
     # convert unix time list in response dict to datetime list for x-axis in figure
     real_time = [datetime.datetime.fromtimestamp(int(x)).strftime('%Y-%m-%d') for x in df['t']]
+
+    return df, real_time
+
+
+def gather_eps_data():
+    """Gathers the data required to make an EPS chart"""
+
+    df = pd.read_json(f'https://finnhub.io/api/v1/stock/earnings?symbol=TSLA&token={config.finn_key}')
+
+    return df
+
+
+def gather_sma_data():
+    """Returns data needed to create a moving average chart"""
+
+    current_time_unix = int(time.time())
+    # one month doesn't have enough data for 20 trading days sometimes.
+    one_month_ago_unix = int(time.time() - 2_592_000)
+    two_months_ago_unix = one_month_ago_unix - 2_592_000
+
+    df = pd.read_json(f'https://finnhub.io/api/v1/stock/candle?symbol=TSLA'
+                      f'&resolution=D&from={two_months_ago_unix}&to={current_time_unix}&token={config.finn_key}')
+
+    # last 20 closing prices for time range specified
+    last_20_prices = [i for i in df['c'][-20:]]
+    moving_averages_20_day = []
+
+    total = 0
+    i = 1
+
+    # calculate moving average values
+    for price in last_20_prices:
+        total += price
+        sma_20 = total / i
+        moving_averages_20_day.append(sma_20)
+        i += 1
+
+    return last_20_prices, moving_averages_20_day
+
+
+def gather_recommend_data():
+    """Gathers information needed to create recommendation chart"""
+
+    df = pd.read_json(f'https://finnhub.io/api/v1/stock/recommendation?symbol=TSLA&token={config.finn_key}')
+
+    return df
+
+
+def gather_news():
+    """Gathers the latest news on Tesla"""
+
+    # unix times:
+    real_time_unix = int(time.time())
+    one_month_ago_unix = int(time.time()) - 2_592_000
+
+    # convert unix time to datetime:
+    real_time = datetime.datetime.fromtimestamp(real_time_unix)
+    one_month_ago = datetime.datetime.fromtimestamp(one_month_ago_unix)
+
+    # format datetime for API query
+    real_time = real_time.strftime('%Y-%m-%d')
+    one_month_ago = one_month_ago.strftime('%Y-%m-%d')
+
+    # query to receive last month of news on Tesla:
+    df = pd.read_json(f'https://finnhub.io/api/v1/company-news?symbol=TSLA&from={one_month_ago}&to={real_time}&token={config.finn_key}')
+
+    return df
+
+
+def create_candle_chart():
+    """Returns a candlestick chart in HTML format using a stock API and Pandas/Plotly"""
+
+    df, real_time = gather_candle_data()
 
     # create figure using 'OHLC' Tesla data
     fig = go.Figure(data=[go.Candlestick(
@@ -62,7 +135,7 @@ def create_candle_chart():
 def create_eps_chart():
     """Returns a EPS surprise chart in HTML format"""
 
-    df = pd.read_json(f'https://finnhub.io/api/v1/stock/earnings?symbol=TSLA&token={config.finn_key}')
+    df = gather_eps_data()
 
     # create custom ticks/labels for better UX/UI
     custom_x_ticks = [i for i in range(1, 5)]
@@ -98,34 +171,6 @@ def create_eps_chart():
 
     chart = fig.to_html(full_html=False, default_height=600, default_width=1200)
     return chart
-
-
-def gather_sma_data():
-    """Returns data needed to create a moving average chart"""
-
-    current_time_unix = int(time.time())
-    # one month doesn't have enough data for 20 trading days sometimes.
-    one_month_ago_unix = int(time.time() - 2_592_000)
-    two_months_ago_unix = one_month_ago_unix - 2_592_000
-
-    df = pd.read_json(f'https://finnhub.io/api/v1/stock/candle?symbol=TSLA'
-                      f'&resolution=D&from={two_months_ago_unix}&to={current_time_unix}&token={config.finn_key}')
-
-    # last 20 closing prices for time range specified
-    last_20_prices = [i for i in df['c'][-20:]]
-    moving_averages_20_day = []
-
-    total = 0
-    i = 1
-
-    # calculate moving average values
-    for price in last_20_prices:
-        total += price
-        sma_20 = total / i
-        moving_averages_20_day.append(sma_20)
-        i += 1
-
-    return last_20_prices, moving_averages_20_day
 
 
 def create_sma_chart():
@@ -183,16 +228,10 @@ def create_sma_chart():
     return chart
 
 
-def gather_recommend_data(): #  save just incase
-    """Gathers information needed to create recommendation chart"""
-
-    df = pd.read_json(f'https://finnhub.io/api/v1/stock/recommendation?symbol=TSLA&token={config.finn_key}')
-
-
 def create_recommend_chart():
     """Returns a recommendation trend chart in HTML format"""
 
-    df = pd.read_json(f'https://finnhub.io/api/v1/stock/recommendation?symbol=TSLA&token={config.finn_key}')
+    df = gather_recommend_data()
 
     # create lists of latest 6 months of data of recommendations. (reversed with '[::-1]' to get oldest data first)
     strong_buy = [i for i in df['strongBuy'][:6][::-1]]
@@ -220,27 +259,6 @@ def create_recommend_chart():
 
     chart = fig.to_html(full_html=False, default_height=800, default_width=1200)
     return chart
-
-
-def gather_news():
-    """Gathers the latest news on Tesla"""
-
-    # unix times:
-    real_time_unix = int(time.time())
-    one_month_ago_unix = int(time.time()) - 2_592_000
-
-    # convert unix time to datetime:
-    real_time = datetime.datetime.fromtimestamp(real_time_unix)
-    one_month_ago = datetime.datetime.fromtimestamp(one_month_ago_unix)
-
-    # format datetime for API query
-    real_time = real_time.strftime('%Y-%m-%d')
-    one_month_ago = one_month_ago.strftime('%Y-%m-%d')
-
-    # query to receive last month of news on Tesla:
-    df = pd.read_json(f'https://finnhub.io/api/v1/company-news?symbol=TSLA&from={one_month_ago}&to={real_time}&token={config.finn_key}')
-
-    return df
 
 
 def create_news_package():
