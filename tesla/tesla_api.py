ef27aaef0482f4a5cacf8b14dc159514e1176c83
cudderson <codered1227@gmail.com>
2021-02-15 17:26:47 -0600

    set environment vars

--- tesla/tesla_api.py
+++ tesla/tesla_api.py
@@ -5,12 +5,14 @@ import config
 import time
 import datetime
 import pandas as pd
+import os
 
+finn_key = os.environ["FINN_KEY"]
 
 def current_price():
     """Returns the current price of Tesla"""
 
-    r = requests.get(f'https://finnhub.io/api/v1/quote?symbol=TSLA&token={config.finn_key}')
+    r = requests.get(f'https://finnhub.io/api/v1/quote?symbol=TSLA&token={finn_key}')
     res = r.json()
 
     return res['c']
@@ -24,7 +26,7 @@ def gather_candle_data():
     six_months_ago_unix = int(time.time() - 15_780_000)
 
     df = pd.read_json(f'https://finnhub.io/api/v1/stock/candle?symbol=TSLA'
-                      f'&resolution=D&from={six_months_ago_unix}&to={current_time_unix}&token={config.finn_key}')
+                      f'&resolution=D&from={six_months_ago_unix}&to={current_time_unix}&token={finn_key}')
 
     # convert unix time list in response dict to datetime list for x-axis in figure
     real_time = [datetime.datetime.fromtimestamp(int(x)).strftime('%Y-%m-%d') for x in df['t']]
@@ -35,7 +37,7 @@ def gather_candle_data():
 def gather_eps_data():
     """Gathers the data required to make an EPS chart"""
 
-    df = pd.read_json(f'https://finnhub.io/api/v1/stock/earnings?symbol=TSLA&token={config.finn_key}')
+    df = pd.read_json(f'https://finnhub.io/api/v1/stock/earnings?symbol=TSLA&token={finn_key}')
 
     return df
 
@@ -49,7 +51,7 @@ def gather_sma_data():
     two_months_ago_unix = one_month_ago_unix - 2_592_000
 
     df = pd.read_json(f'https://finnhub.io/api/v1/stock/candle?symbol=TSLA'
-                      f'&resolution=D&from={two_months_ago_unix}&to={current_time_unix}&token={config.finn_key}')
+                      f'&resolution=D&from={two_months_ago_unix}&to={current_time_unix}&token={finn_key}')
 
     # last 20 closing prices for time range specified
     last_20_prices = [i for i in df['c'][-20:]]
@@ -71,7 +73,7 @@ def gather_sma_data():
 def gather_recommend_data():
     """Gathers information needed to create recommendation chart"""
 
-    df = pd.read_json(f'https://finnhub.io/api/v1/stock/recommendation?symbol=TSLA&token={config.finn_key}')
+    df = pd.read_json(f'https://finnhub.io/api/v1/stock/recommendation?symbol=TSLA&token={finn_key}')
 
     return df
 
@@ -92,6 +94,6 @@ def gather_news():
     one_month_ago = one_month_ago.strftime('%Y-%m-%d')
 
     # query to receive last month of news on Tesla:
-    df = pd.read_json(f'https://finnhub.io/api/v1/company-news?symbol=TSLA&from={one_month_ago}&to={real_time}&token={config.finn_key}')
+    df = pd.read_json(f'https://finnhub.io/api/v1/company-news?symbol=TSLA&from={one_month_ago}&to={real_time}&token={finn_key}')
 
     return df
