cdee24c4ada184dd63016fab9982e22d4fa40109
cudderson <codered1227@gmail.com>
2022-01-08 12:05:21 -0600

    modularize React component directory structure to begin api integration

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,3 +1,5 @@
+import testGetCurrentPrice from "./api/TestGetCurrentPrice.js";
+
 function App() {
 
   const testAPI = async () => {
@@ -11,19 +13,9 @@ function App() {
       console.log(error);
     }
   }
-  // testAPI();
+  testAPI();
 
-  const testGetCurrentPrice = async () => {
-    const url = 'http://localhost:8000/api/test-current-price';
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data); // successfully logs the current price of tesla :)
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
+
   testGetCurrentPrice();
 
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App.js';
+
+// apparently, <React.StrictMode/> will cause our component to render twice (dev mode only, not prod) 
 
 ReactDOM.render(
   <React.StrictMode>
