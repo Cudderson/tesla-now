3dc7dbb39484f2395bd1a48211e0e227edb26e9c
cudderson <codered1227@gmail.com>
2022-01-08 11:25:50 -0600

    create api/urls.py to bundle api URLS for app-root urls.py

---
+++ tesla/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+# these patterns are imported by parent urls.py 
+urlpatterns = [
+  path('hello-world/', views.hello_world, name='hello-world'),
+  path('test-current-price/', views.test_get_current_price, name='test-current-price')
+]
--- tesla_now/urls.py
+++ tesla_now/urls.py
@@ -1,27 +1,20 @@
-"""tesla_now URL Configuration
+"""
+tesla_now URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework import routers
-from tesla.api import views
+
+# api urls
+from tesla.api.urls import urlpatterns as api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tesla.urls')),
-    # combine into viewset when possible
-    path('api/hello-world', views.hello_world, name='hello-world'),
-    path('api/test-current-price', views.test_get_current_price, name='test-current-price')
+
+    # sets 'api/' as the basename for api urls
+    path('api/', include(api_urls)),
 ]
