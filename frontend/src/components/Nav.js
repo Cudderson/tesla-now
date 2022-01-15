3ee3f0d560330b56c09abcac331dc41efe0adb91
cudderson <codered1227@gmail.com>
2022-01-15 13:11:23 -0600

    create initial styles for horizontal nav

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,27 +1,39 @@
 import { Link } from "react-router-dom";
+import styles from "./../styles/Nav.module.css";
 
 const Nav = () => {
   return (
     <nav>
+      <h3>Tesla Now</h3>
       <ul>
         <li>
-          <Link to="/candlestick">Candlestick</Link>
+          <Link className={styles["link"]} to="/candlestick">
+            Candlestick
+          </Link>
         </li>
         <li>
-          <Link to="/eps">Earnings Per Share</Link>
+          <Link className={styles["link"]} to="/eps">
+            Earnings Per Share
+          </Link>
         </li>
         <li>
-          <Link to="/sma">Simple Moving Average</Link>
+          <Link className={styles["link"]} to="/sma">
+            Simple Moving Average
+          </Link>
         </li>
         <li>
-          <Link to="/recommendations">Analyst Recommendations</Link>
+          <Link className={styles["link"]} to="/recommendations">
+            Analyst Recommendations
+          </Link>
         </li>
         <li>
-          <Link to="/news">News</Link>
+          <Link className={styles["link"]} to="/news">
+            News
+          </Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Nav;
