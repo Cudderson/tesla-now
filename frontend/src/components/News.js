eec4477ecbfbf8370b905c55ca53b4829b16b13d
cudderson <codered1227@gmail.com>
2022-01-17 14:06:24 -0600

    add default news styles

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -1,8 +1,11 @@
+import styles from './../styles/News.module.css';
+
 const News = (props) => {
   console.log(props.newsData);
+  console.log(Object.keys(props.newsData).length);
 
   return (
-    <>
+    <div className={styles['news-outer']}>
       {/* let's make sure all the data was serialized properly */}
       <img src={props.newsData.images[0]} alt='missing photo' style={{width: '200px'}}></img>
       <h4>{props.newsData.times_posted[0]}</h4>
@@ -10,7 +13,7 @@ const News = (props) => {
       <h4>{props.newsData.sources[0]}</h4>
       <h3>{props.newsData.headlines[0]}</h3>
       <h5>{props.newsData.summaries[0]}</h5>
-    </>
+    </div>
   )
 }
 
