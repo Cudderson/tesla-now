0e5701841d99f392fe6d1a03f9f6bda68337f765
cudderson <codered1227@gmail.com>
2022-01-18 12:48:05 -0600

    style news articles, make responsive to mobile and desktop

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -6,26 +6,17 @@ const News = (props) => {
 
   return (
     <div className={styles['news-outer']}>
-      {/* let's make sure all the data was serialized properly */}
-      {/* <img src={props.newsData.images[0]} alt='missing photo' style={{width: '200px'}}></img>
-      <h4>{props.newsData.times_posted[0]}</h4>
-      <h4>{props.newsData.urls[0]}</h4>
-      <h4>{props.newsData.sources[0]}</h4>
-      <h3>{props.newsData.headlines[0]}</h3>
-      <h5>{props.newsData.summaries[0]}</h5> */}
       <div className={styles['news-inner']}>
-        {/* new method schema, don't remove above until sure all working */}
         {props.newsData ? (
           props.newsData.map((news) => (
             <div key={news.time_posted.toString()} className={styles['news']}>
               <div className={styles['image-container']}>
                 {news.image ? <img src={news.image}/> : <img src={tesla_default_image} /> }
               </div>
-              <h6>{news.time_posted}</h6>
-              <h5>{news.source}</h5>
-              <h2>{news.headline}</h2>
-              <h3>{news.summary}</h3>
-              <a href={news.url} target="_blank">Go To Article</a>
+              <h2 className={styles['headline']}>{news.headline}</h2>
+              <h5 style={{color: 'gray', margin: 0, padding: 0}}>{news.source} {news.time_posted}</h5>
+              <p className={styles['summary']}>{news.summary}</p>
+              <a className={styles['link']} href={news.url} target="_blank">Go To Article</a>
             </div>
           ))
         ) : null}
