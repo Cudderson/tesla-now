29bae569056466dd13037188e7fb3c8f31c63107
cudderson <codered1227@gmail.com>
2022-01-18 14:19:02 -0600

    add fonts to index.html, apply to News component

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -1,28 +1,43 @@
-import styles from './../styles/News.module.css';
-import tesla_default_image from './../tesla-logo.png';
+import styles from "./../styles/News.module.css";
+import tesla_default_image from "./../tesla-logo.png";
 
 const News = (props) => {
   console.log(props.newsData);
 
   return (
-    <div className={styles['news-outer']}>
-      <div className={styles['news-inner']}>
-        {props.newsData ? (
-          props.newsData.map((news) => (
-            <div key={news.time_posted.toString()} className={styles['news']}>
-              <div className={styles['image-container']}>
-                {news.image ? <img src={news.image}/> : <img src={tesla_default_image} /> }
+    <div className={styles["news-outer"]}>
+      <div className={styles["news-inner"]}>
+        {props.newsData
+          ? props.newsData.map((news) => (
+              <div key={news.time_posted.toString()} className={styles["news"]}>
+                <div className={styles["image-container"]}>
+                  {news.image ? (
+                    <img src={news.image} />
+                  ) : (
+                    <img src={tesla_default_image} />
+                  )}
+                </div>
+                <h2 className={styles["headline"]}>
+                  <a href={news.url} target="_blank">{news.headline}</a>
+                </h2>
+                <div className={styles["source"]}>
+                  <span>{news.source}</span>
+                  <span className={styles["time-posted"]}>
+                    {news.time_posted}
+                  </span>
+                </div>
+                <p className={styles["summary"]}>{news.summary}</p>
+                <span>
+                  <a className={styles["link"]} href={news.url} target="_blank">
+                    Go To Article
+                  </a>
+                </span>
               </div>
-              <h2 className={styles['headline']}>{news.headline}</h2>
-              <h5 style={{color: 'gray', margin: 0, padding: 0}}>{news.source} {news.time_posted}</h5>
-              <p className={styles['summary']}>{news.summary}</p>
-              <a className={styles['link']} href={news.url} target="_blank">Go To Article</a>
-            </div>
-          ))
-        ) : null}
+            ))
+          : null}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default News;
