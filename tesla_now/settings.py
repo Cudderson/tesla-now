134c722797306709264620673ca11495b86de270
cudderson <codered1227@gmail.com>
2022-01-22 14:37:14 -0600

    remove more deprecated django code

--- tesla_now/settings.py
+++ tesla_now/settings.py
@@ -51,9 +51,6 @@ ALLOWED_HOSTS = [
     'tesla-now.herokuapp.com',
 ]
 
-
-# Application definition
-
 INSTALLED_APPS = [
     # My Apps
     'tesla',
@@ -149,15 +146,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-# STATIC_URL = '/static/'
-
-# # for prod
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # heroku settings
 import django_heroku
